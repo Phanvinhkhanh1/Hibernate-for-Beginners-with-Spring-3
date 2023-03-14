613a79d183fd6330738177e3f5a50af5a6f641fb
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-14 22:11:58 +0700

    Injecting Custom Application properties with @Value

--- Spring-Hibernate/src/main/java/com/selfStudy/SpringHibernate/controller/FunRestController.java
+++ Spring-Hibernate/src/main/java/com/selfStudy/SpringHibernate/controller/FunRestController.java
@@ -1,10 +1,17 @@
 package com.selfStudy.SpringHibernate.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${user.firstname}")
+    private String firstName;
+
+    @Value("${user.lastname}")
+    private String lastName;
 
     @GetMapping("/funController")
     public String getName() {
@@ -14,5 +21,10 @@ public class FunRestController {
     @GetMapping("/secondController")
     public String getInfo() {
         return "Phan Vinh Khanh";
+    }
+
+    @GetMapping("/userInfo")
+    public String getUserInfo() {
+        return firstName + " " + lastName;
     }
 }
