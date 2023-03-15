72b73c3f72aee986c209c96502d1c1c05f035266
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-15 20:18:24 +0700

    @Lazy initialization annotation

--- Spring-Hibernate/src/main/java/com/selfStudy/SpringHibernate/controller/RestController.java
+++ Spring-Hibernate/src/main/java/com/selfStudy/SpringHibernate/controller/RestController.java
@@ -2,13 +2,19 @@ package com.selfStudy.SpringHibernate.controller;
 
 import com.selfStudy.SpringHibernate.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
-    @Autowired
     private Coach theCoach;
+
+    @Autowired
+    public RestController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor : " + getClass().getSimpleName());
+        this.theCoach = theCoach;
+    }
 
     @GetMapping("/info")
     public String getInfo() {
--- Spring-Hibernate/src/main/java/com/selfStudy/SpringHibernate/impl/TrackCoach.java
+++ Spring-Hibernate/src/main/java/com/selfStudy/SpringHibernate/impl/TrackCoach.java
@@ -1,19 +1,17 @@
 package com.selfStudy.SpringHibernate.impl;
 
 import com.selfStudy.SpringHibernate.Coach;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
-public class CricketCoach implements Coach {
+public class TrackCoach implements Coach {
 
-    public CricketCoach() {
+    public TrackCoach() {
         System.out.println("In constructor : " + getClass().getSimpleName());
     }
 
     @Override
     public String getDailyWorkout() {
-        return "Learn Chinese and English everyday";
+        return "TrackCoach";
     }
 }
