eb91f475cb5eb198c397a953b60eb71c2b19cd5f
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-22 06:45:44 +0700

    Retrieve All Objects in JPA

--- 3-Hibernate-JPA-CRUD/crudAPI/src/main/java/com/springhibernate/crudAPI/daoImpl/StudentDAOImpl.java
+++ 3-Hibernate-JPA-CRUD/crudAPI/src/main/java/com/springhibernate/crudAPI/daoImpl/StudentDAOImpl.java
@@ -3,9 +3,12 @@ package com.springhibernate.crudAPI.daoImpl;
 import com.springhibernate.crudAPI.dao.StudentDAO;
 import com.springhibernate.crudAPI.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -22,5 +25,11 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findStudentById(int id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> studentTypedQuery = entityManager.createQuery("SELECT s FROM Student s order by s.lastName", Student.class);
+        return studentTypedQuery.getResultList();
     }
 }
