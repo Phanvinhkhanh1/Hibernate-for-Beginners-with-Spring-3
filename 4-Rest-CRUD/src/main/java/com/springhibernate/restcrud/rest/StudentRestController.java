c62cd0a001fbbd11196df456eb512a4aac6d8fba
Phan Vinh Khanh <Khanh161297@gmail.com>
2023-03-22 21:44:02 +0700

    @PathVariable in Springboot

--- 4-Rest-CRUD/src/main/java/com/springhibernate/restcrud/rest/StudentRestController.java
+++ 4-Rest-CRUD/src/main/java/com/springhibernate/restcrud/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.springhibernate.restcrud.rest;
 
 import com.springhibernate.restcrud.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,17 +11,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/info")
-    private List<Student> getListStudents() {
-        List<Student> students = new ArrayList<>();
+    List<Student> students = new ArrayList<>();
 
+    @PostConstruct
+    private void loadData() {
         students.add(new Student("Phan Thi", "Phuong"));
         students.add(new Student("Phan Vinh", "Tung"));
         students.add(new Student("Phan Vinh", "Khanh"));
+    }
 
-        return students;
+    @GetMapping("/students/info")
+    private List<Student> getListStudents() {
+        return this.students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    private Student getStudentById(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 }
